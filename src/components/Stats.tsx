b56722d197875ec3099482976eb1cc754dd7355e
Nate <rewq2502@gmail.com>
2022-11-26 13:12:43 +1300

    Added stats page with standard reporting

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,9 +1,80 @@
+import { useHabitStore, useHabitEntryStore } from "../store/store"
+
+import StatsCalendar from "./StatsCalendar"
+
+import Habit from '../interfaces/Habit.interface'
+import HabitEntry from '../interfaces/HabitEntry.interface'
 
 
 function Stats() {
+  const habits:Habit[] = useHabitStore((state) => state.habits)
+  const habitEntriesHist:HabitEntry[] = useHabitEntryStore((state) => state.habitEntriesHist)
+
+  const getEntriesHistForHabitId = (id:string) => {
+    return habitEntriesHist.filter((h) => h.habit_id === id)
+  }
+
+  const calcDayStreak = (entries:HabitEntry[]) => {
+    let yesterday = new Date()
+    yesterday.setDate(yesterday.getDate() - 1)
+    yesterday.setHours(0,0,0,0)
+    let count = 0
+    let sortedArr = entries.sort((a, b) => b.timestamp - a.timestamp)
+
+    for(let i=0; i<sortedArr.length; i++) {
+      if(!!sortedArr[i].complete && yesterday.getTime() - sortedArr[i].timestamp === i*86400000) {
+        count++
+      } else { break }
+    }
+
+    return count
+  }
+
+  const calcDaysSinceCreation = (createdTimestamp:any) => {
+    const createdDate = new Date(createdTimestamp)
+    const today = new Date()
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    const utc1 = Date.UTC(createdDate.getFullYear(), createdDate.getMonth(), createdDate.getDate());
+    const utc2 = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY)
+  }
+
+  const habitStatsText = (createdTimestamp:number|object, entries:HabitEntry[]) => {
+    const entriesCopy = [...entries]
+    if(!entries.length) { return "This was created today. Stats will show from tomorrow" }
+
+    const daysSinceCreation = calcDaysSinceCreation(createdTimestamp)
+    const daysCompleted = entriesCopy.filter((e) => !!e.complete).length
+    const completePercent = Math.floor((daysCompleted/daysSinceCreation)*100) + "%"
+    const dayStreak = calcDayStreak(entriesCopy)
+
+    let text = <>You have completed this <b>{daysCompleted}</b> time{daysCompleted===1?'':'s'} since starting {daysSinceCreation} days ago ({completePercent}) 
+    and are on a <b>{dayStreak}</b> day streak</>
+
+    return text
+  }
+
   return (
-    <div>
-      test
+    <div className="container">
+      {habits && habits.map(habit => (
+        <div key={habit.id} className="container col-xl-10 col-xxl-8 px-4 py-3 border">
+          <div className="row align-items-center g-lg-4 py-4">
+            <div>
+              <h2 className="h2">{habit.title}</h2>
+            </div>
+            <div>
+              {habitStatsText(habit.timestamp, getEntriesHistForHabitId(habit.id))}
+            </div>
+            {getEntriesHistForHabitId(habit.id).length > 0 && (
+              <div>
+                <StatsCalendar entries={getEntriesHistForHabitId(habit.id)} />
+              </div>
+            )}
+
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ src/components/StatsCalendar.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+
+import { v4 as uuidv4 } from 'uuid'
+
+import HabitEntry from '../interfaces/HabitEntry.interface'
+
+
+function StatsCalendar({ entries }:{entries: HabitEntry[]}) {
+  const [ tiles, setTiles ] = useState(Array.from({length: 30}, (v, k) => {
+    let yesterday = new Date()
+    yesterday.setDate(yesterday.getDate() - 1)
+    yesterday.setHours(0,0,0,0)
+
+    return {
+      tileId: uuidv4(),
+      timestamp: yesterday.getTime() - k*86400000,
+      complete: false
+    }
+  }).reverse())
+
+  useEffect(() => {
+    const newTiles = [...tiles].map(tile => {
+      const matchingEntry = [...entries].find(entry => entry.timestamp === tile.timestamp)
+      if(matchingEntry) { tile.complete = matchingEntry.complete }
+      return tile
+    })
+    setTiles(newTiles)
+  }, [entries])
+
+  const Tile = ({ timestamp, complete }:{ timestamp:number, complete:boolean }) => {
+    const date = new Date(timestamp)
+
+    return (
+      <div className={`stats-tile col-sm ${complete?'complete':''}`} title={date.toDateString()}></div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        {tiles && tiles.map((tile, i) => (
+          <Tile key={tile.tileId} {...tile} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default StatsCalendar
